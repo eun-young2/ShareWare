2342482001e6356e012766866f893f976445e03b
eun-young2 <grit_ey@naver.com>
2024-10-16 15:40:35 +0900

    회원가입, 로그인 기능 구현 - 은영

--- Frontend/shareware/lib/login_page.dart
+++ Frontend/shareware/lib/login_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_33/main.dart';
 import 'package:flutter_application_33/signup_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -7,8 +10,52 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   // 사용자와 관리자를 구분하기 위한 상태 변수
   bool isAdminLogin = false;
+
+  // 색상 정의
+  Color customGreen = Color(0xFFAFD485); // AFD485 색상
+  Color customGray = Color(0xFF4A4A4A); // 4A4A4A 색상
+
+  Future<void> login() async {
+    final String apiUrl = 'http://192.168.70.43:3000/user/login'; // 로그인 API URL
+
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'userid': usernameController.text,
+        'password': passwordController.text,
+      }),
+    );
+
+    // 응답 처리
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      // 로그인 성공 및 role 체크
+      if (isAdminLogin && data['role'] != 'admin') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('관리자로 로그인할 수 없습니다.')),
+        );
+      } else {
+        print('로그인 성공');
+        // 추가 로직 (예: 대시보드로 이동)
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MainPage()),
+        ); // 대시보드로 이동
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('로그인 실패')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +70,7 @@ class _LoginPageState extends State<LoginPage> {
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
-            
+
             // 사용자와 관리자 선택 버튼
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -35,9 +82,14 @@ class _LoginPageState extends State<LoginPage> {
                     });
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: isAdminLogin ? Colors.grey : Colors.green, // primary 대신 backgroundColor 사용
+                    backgroundColor: isAdminLogin
+                        ? customGray
+                        : customGreen, // primary 대신 backgroundColor 사용
                   ),
-                  child: Text('사용자 로그인'),
+                  child: Text(
+                    '사용자 로그인',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
                 SizedBox(width: 10),
                 ElevatedButton(
@@ -47,16 +99,19 @@ class _LoginPageState extends State<LoginPage> {
                     });
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: isAdminLogin ? Colors.green : Colors.grey, // primary 대신 backgroundColor 사용
+                    backgroundColor: isAdminLogin
+                        ? customGreen
+                        : customGray, // primary 대신 backgroundColor 사용
                   ),
-                  child: Text('관리자 로그인'),
+                  child: Text('관리자 로그인', style: TextStyle(color: Colors.white)),
                 ),
               ],
             ),
-            
+
             SizedBox(height: 20),
-            
+
             TextField(
+              controller: usernameController,
               decoration: InputDecoration(
                 labelText: isAdminLogin ? '관리자 아이디' : '아이디', // 선택에 따라 레이블 변경
                 border: OutlineInputBorder(),
@@ -64,6 +119,7 @@ class _LoginPageState extends State<LoginPage> {
             ),
             SizedBox(height: 20),
             TextField(
+              controller: passwordController,
               decoration: InputDecoration(
                 labelText: '비밀번호',
                 border: OutlineInputBorder(),
@@ -74,6 +130,7 @@ class _LoginPageState extends State<LoginPage> {
             ElevatedButton(
               onPressed: () {
                 // 로그인 로직 (사용자/관리자에 따른 처리 추가 예정)
+                login();
               },
               child: Text('로그인'),
             ),
@@ -82,7 +139,8 @@ class _LoginPageState extends State<LoginPage> {
                 // 회원가입 페이지로 이동
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => SignupPage()), // SignupPage로 이동
+                  MaterialPageRoute(
+                      builder: (context) => SignupPage()), // SignupPage로 이동
                 );
               },
               child: Text('회원가입'),
