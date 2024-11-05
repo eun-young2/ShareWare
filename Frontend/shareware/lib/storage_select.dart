2e32178435718fcb11dfe0cb3a4268130a16f5a9
rlarbals0000 <rbals0710@gmail.com>
2024-11-05 16:44:14 +0900

    지도복구 sotrage_select.dart

--- Frontend/shareware/lib/storage_select.dart
+++ Frontend/shareware/lib/storage_select.dart
@@ -40,63 +40,91 @@ class _KakaoMapTestState extends State<KakaoMapTest> {
     });
 
     // 현재 위치로 지도 중심 이동
-    _webViewController.runJavascript(
-      'map.setCenter(new kakao.maps.LatLng($_currentLat, $_currentLon));'
-    );
+    if (_webViewController != null) {
+      _webViewController.runJavascript(
+          'map.setCenter(new kakao.maps.LatLng($_currentLat, $_currentLon));');
+    }
   }
 
+  // 위치 정보 권한 및 서비스 확인 후 현재 위치 가져오기
   Future<void> _getCurrentLocation() async {
     setState(() {
       _isLoading = true; // 로딩 시작
     });
 
-    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // 위치 서비스 활성화 여부 확인
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       setState(() {
         _isLoading = false; // 로딩 종료
       });
-      return;
+      return Future.error('Location services are disabled.');
     }
 
-    LocationPermission permission = await Geolocator.checkPermission();
+    // 위치 권한 확인
+    permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
-      if (permission != LocationPermission.whileInUse && permission != LocationPermission.always) {
+      if (permission == LocationPermission.denied) {
         setState(() {
           _isLoading = false; // 로딩 종료
         });
-        return;
+        return Future.error('Location permissions are denied.');
       }
     }
 
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        _isLoading = false; // 로딩 종료
+      });
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // 현재 위치 가져오기
     Position position = await Geolocator.getCurrentPosition();
-    _updateCurrentLocation(position.latitude, position.longitude);
+    _updateCurrentLocation(position.latitude, position.longitude); // 현재 위치 업데이트
+
+    // 위치가 로드된 후 지도의 중심을 업데이트
+    if (_webViewController != null) {
+      _webViewController.runJavascript(
+        'map.setCenter(new kakao.maps.LatLng(${position.latitude}, ${position.longitude}));',
+      );
+    }
 
     setState(() {
       _isLoading = false; // 로딩 종료
     });
   }
 
+  // 모든 창고 좌표 및 정보 API 호출
   Future<List<Warehouse>> fetchAllWarehouseCoordinates() async {
-    final response = await http.get(Uri.parse('${Config.local}/map/all/warehouses'));
+    final response =
+        await http.get(Uri.parse('${Config.local}/map/all/warehouses'));
     if (response.statusCode == 200) {
       final List<dynamic> data = jsonDecode(response.body);
+      // print("API 응답 데이터: $data"); // 응답 데이터 출력
       return data.map((item) => Warehouse.fromJson(item)).toList();
     } else {
       throw Exception('창고를 찾을 수 없습니다');
     }
   }
 
+  // 창고 이름을 통해 특정 창고 좌표 및 정보 가져오는 API 호출
   Future<Warehouse?> fetchWarehouseByName(String name) async {
     final response = await http.get(Uri.parse('${Config.local}/map/$name'));
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = jsonDecode(response.body);
       return Warehouse.fromJson(data);
     } else {
-      return null;
+      return null; // 창고를 찾을 수 없을 경우 null 반환
     }
   }
 
+  // 32번째부터 10개 창고 선택
   List<Warehouse> _selectSubsetOfWarehouses(List<Warehouse> warehouses) {
     if (warehouses.length < 32) {
       return []; // 데이터가 32개 미만인 경우 빈 리스트 반환
@@ -104,6 +132,7 @@ class _KakaoMapTestState extends State<KakaoMapTest> {
     return warehouses.skip(32).take(10).toList();
   }
 
+  // 검색된 창고 마커 추가 스크립트
   String _generateSearchMarkerScript(Warehouse warehouse) {
     return '''
       var searchMarkerPosition = new kakao.maps.LatLng(${warehouse.lat}, ${warehouse.lon});
@@ -115,11 +144,14 @@ class _KakaoMapTestState extends State<KakaoMapTest> {
     ''';
   }
 
+  // 32번째부터 10개 창고 마커 추가 스크립트
   String _generateMarkersScript() {
     StringBuffer script = StringBuffer();
     script.writeln('var markers = [];'); // 마커 배열 초기화
 
+    // 32번째부터 10개만 표시
     List<Warehouse> limitedWarehouses = _selectSubsetOfWarehouses(_warehouses);
+
     for (var i = 0; i < limitedWarehouses.length; i++) {
       var warehouse = limitedWarehouses[i];
       script.writeln('''
@@ -135,50 +167,76 @@ class _KakaoMapTestState extends State<KakaoMapTest> {
     return script.toString();
   }
 
+  // 마커 초기화
   void _clearMarkers() {
-    _webViewController.runJavascript(
-      "if (markers) { markers.forEach(marker => marker.setMap(null)); markers = []; }"
-    ).catchError((error) {
-      print("기존 마커 제거 오류: $error");
-    });
+    if (_webViewController != null) {
+      _webViewController
+          .runJavascript(
+              "if (markers) { markers.forEach(marker => marker.setMap(null)); markers = []; }")
+          .then((_) {
+        print("기존 마커 제거 성공");
+      }).catchError((error) {
+        print("기존 마커 제거 오류: $error");
+      });
+    } else {
+      print("WebViewController가 아직 초기화되지 않았습니다."); // 디버깅 메시지
+    }
   }
 
+  // 여러 개의 마커를 지도에 추가하는 함수
   void _addMarkers() {
-    final markersScript = _generateMarkersScript();
-    _webViewController.runJavascript(markersScript).catchError((error) {
-      print("JavaScript 실행 오류: $error");
-    });
+    if (_webViewController != null) {
+      final markersScript = _generateMarkersScript();
+      _webViewController.runJavascript(markersScript).then((_) {
+        print("JavaScript 실행 성공: 마커 추가됨");
+      }).catchError((error) {
+        print("JavaScript 실행 오류: $error");
+      });
+    } else {
+      print("WebViewController가 아직 초기화되지 않았습니다.");
+    }
   }
 
+  // 모든 창고 좌표 및 정보 로드
   Future<void> _loadAllWarehouseCoordinates() async {
     try {
-      _warehouses = await fetchAllWarehouseCoordinates();
-      List<Warehouse> selectedWarehouses = _selectSubsetOfWarehouses(_warehouses);
+      _warehouses = await fetchAllWarehouseCoordinates(); // 모든 창고 정보 가져오기
+      print("불러온 창고 수: ${_warehouses.length}"); // 불러온 창고 수 출력
+
+      // 32번째부터 10개의 창고만 선택하여 마커와 리스트에 표시
+      List<Warehouse> selectedWarehouses =
+          _selectSubsetOfWarehouses(_warehouses);
 
       setState(() {
-        _warehouses = selectedWarehouses;
-        _addMarkers();
+        _warehouses = selectedWarehouses; // 바텀시트에 표시할 창고 정보를 업데이트
+        _addMarkers(); // 마커 추가
       });
     } catch (e) {
       print('좌표를 가져오는 중 오류 발생: $e');
     }
   }
 
+  // 검색을 통한 창고 마커 및 지도 중심 이동
   Future<void> _searchWarehouse(String name) async {
     try {
       _clearMarkers();
+
       final warehouse = await fetchWarehouseByName(name);
       if (warehouse != null) {
         setState(() {
-          _warehouses = [warehouse];
+          _warehouses = [warehouse]; // 검색된 창고만 마커로 표시
 
+          // 검색된 창고 위치에 마커 추가
           final searchMarkerScript = _generateSearchMarkerScript(warehouse);
-          _webViewController.runJavascript(searchMarkerScript).catchError((error) {
+          _webViewController.runJavascript(searchMarkerScript).then((_) {
+            print("검색된 창고 마커 추가 성공");
+          }).catchError((error) {
             print("검색된 창고 마커 추가 오류: $error");
           });
 
+          // 검색된 창고 위치로 지도 중심 이동
           _webViewController.runJavascript(
-            'map.setCenter(new kakao.maps.LatLng(${warehouse.lat}, ${warehouse.lon}));'
+            'map.setCenter(new kakao.maps.LatLng(${warehouse.lat}, ${warehouse.lon}));',
           );
         });
       } else {
@@ -277,12 +335,13 @@ class _KakaoMapTestState extends State<KakaoMapTest> {
                   children: [
                     GestureDetector(
                       onTap: () {
+                        // 핸들을 클릭하면 기본적인 드래그 동작을 시작할 수 있도록
                         Scrollable.of(context)?.position?.jumpTo(
                             Scrollable.of(context)!.position.pixels + 100);
                       },
                       child: Container(
                         width: 60,
-                        height: 8,
+                        height: 8, // 핸들의 높이를 늘림
                         margin: EdgeInsets.symmetric(vertical: 8),
                         decoration: BoxDecoration(
                           color: Colors.black.withOpacity(0.5),
@@ -304,7 +363,9 @@ class _KakaoMapTestState extends State<KakaoMapTest> {
                                 Text('주소: ${warehouse.address}'),
                                 Text('연락처: ${warehouse.contact}'),
                                 Text('영업시간: ${warehouse.hours}'),
-                                Text('주차 가능 여부: ${warehouse.getParkingAvailability()}'),
+                                Text(
+                                  '주차 가능 여부: ${warehouse.getParkingAvailability()}',
+                                ),
                               ],
                             ),
                             onTap: () => _navigateToWarehouseDetails(warehouse),
